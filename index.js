ebc5da78f5e84bae46b9d77f9f76f242a6efc13d
meeniebeanie <minyen.j@gmail.com>
2016-10-13 17:39:02 +0800

    successfully created slack app on localtunnel. the command is 'lt --port 8765 --subdomain myawesomebot' /login to authorize new team.

--- index.js
+++ index.js
@@ -82,13 +82,12 @@ controller.on('rtm_close', function (bot) {
 // BEGIN EDITING HERE!
 
 controller.on('bot_channel_join', function (bot, message) {
-    bot.reply(message, "I'm here!")
+    bot.reply(message, "I'm here! ✨✨✨")
 });
 
-controller.hears('hello', 'direct_message', function (bot, message) {
-    bot.reply(message, 'Hello!');
+controller.hears(['hello', 'hi', 'hey'], 'direct_message', function (bot, message) {
+    bot.reply(message, 'What\'s up!✨');
 });
-
 
 /**
  * AN example of what could be:
